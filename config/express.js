1bef911466cc40ea8e886c3ba1cdd2403fcd5b56
digibrose <digibrose@googlemail.com>
2016-11-22 18:28:36 +0000

    first commit

---
+++ config/express.js
@@ -0,0 +1,37 @@
+var config = require('./config'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    passport = require('passport'),
+    flash = require('connect-flash'),
+	session = require('express-session');
+	
+module.exports = function() {
+    var app = express();
+	
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(bodyParser.json());
+    app.use(express.static('./public'));
+    
+    app.use(session({
+    saveUninitialized: true,
+    resave: true,
+    secret: 'OurSuperSecretCookieSecret'
+}));
+    
+    app.use(flash());
+
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
+    
+    //use this code before any route definitions
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    require('../app/routes/index.server.routes.js')(app);
+    require('../app/routes/users.server.routes.js')(app);
+
+    return app;
+};
